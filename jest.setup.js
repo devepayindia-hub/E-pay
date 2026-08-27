12b4e8e7e71d14efa54cf96f77f4977ead3fb13d
Your Name <dev.epayindia@gmail.com>
2026-08-27 15:14:52 +0530

    fix(test): add Web API polyfills to jest.setup.js for Jest test execution

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+import { ReadableStream } from 'stream/web';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+global.ReadableStream = ReadableStream;
+
+
